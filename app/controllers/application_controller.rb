d65d1c0fbc552313f49cc8bdd8302361985fb7e6
jaesonwatts <illender.the.great@gmail.com>
2020-06-05 14:49:04 -0600

    helpers added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,5 +17,17 @@ class ApplicationController < Sinatra::Base
   end
 
 
+  helpers do
+    
+    def logged_in
+      !!session[:user_id]
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+  end
+
 
 end
